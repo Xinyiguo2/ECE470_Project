2fb4f0697866f082ef636dc029048ee90a994c64
albertwlchan <54993775+albertwlchan@users.noreply.github.com>
2019-10-20 18:54:50 -0500

    created world map

--- python_avoid_obstacle.py
+++ python_avoid_obstacle.py
@@ -1,66 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 06 22:00:39 2015
-
-@author: Nikolai K.
-"""
-#Import Libraries:
-import vrep                  #V-rep library
+import vrep                 
 import sys
-import time                #used to keep track of time
-import numpy as np         #array library
+import time                
+import numpy as np         
 import math
-import matplotlib as mpl   #used for     x
- plotting
+#import opencv2 as cv2
+ 
 
-#Pre-Allocation
+PI=math.pi  
 
-PI=math.pi  #pi=3.14..., constant
-
-vrep.simxFinish(-1) # just in case, close all opened connections
+vrep.simxFinish(-1) 
 
 clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5)
 
-if clientID!=-1:  #check if client connection successful
+if clientID!=-1:  
     print ('Connected to remote API server')
     
 else:
     print ('Connection not successful')
     sys.exit('Could not connect')
 
-
-#retrieve motor  handles
+# Motor Handles
+#def get_handles
 errorCode,left_motor_handle=vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_oneshot_wait)
 errorCode,right_motor_handle=vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor',vrep.simx_opmode_oneshot_wait)
 
 
-sensor_h=[] #empty list for handles
-sensor_val=np.array([]) #empty array for sensor measurements
+sensor_h=[] 
+sensor_val=np.array([]) 
 
-#orientation of all the sensors: 
+
 sensor_loc=np.array([-PI/2, -50/180.0*PI,-30/180.0*PI,-10/180.0*PI,10/180.0*PI,30/180.0*PI,50/180.0*PI,PI/2,PI/2,130/180.0*PI,150/180.0*PI,170/180.0*PI,-170/180.0*PI,-150/180.0*PI,-130/180.0*PI,-PI/2]) 
 
-#for loop to retrieve sensor arrays and initiate sensors
+
 for x in range(1,16+1):
         errorCode,sensor_handle=vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor'+str(x),vrep.simx_opmode_oneshot_wait)
-        sensor_h.append(sensor_handle) #keep list of handles        
+        sensor_h.append(sensor_handle)      
         errorCode,detectionState,detectedPoint,detectedObjectHandle,detectedSurfaceNormalVector=vrep.simxReadProximitySensor(clientID,sensor_handle,vrep.simx_opmode_streaming)                
-        sensor_val=np.append(sensor_val,np.linalg.norm(detectedPoint)) #get list of values
+        sensor_val=np.append(sensor_val,np.linalg.norm(detectedPoint)) 
         
-
 t = time.time()
 
-
 while (time.time()-t)<60:
-    #Loop Execution
     sensor_val=np.array([])    
     for x in range(1,16+1):
         errorCode,detectionState,detectedPoint,detectedObjectHandle,detectedSurfaceNormalVector=vrep.simxReadProximitySensor(clientID,sensor_h[x-1],vrep.simx_opmode_buffer)                
-        sensor_val=np.append(sensor_val,np.linalg.norm(detectedPoint)) #get list of values
-
+        sensor_val=np.append(sensor_val,np.linalg.norm(detectedPoint)) 
     
-    #controller specific
-    sensor_sq=sensor_val[0:8]*sensor_val[0:8] #square the values of front-facing sensors 1-8
+    sensor_sq=sensor_val[0:8]*sensor_val[0:8] 
         
     min_ind=np.where(sensor_sq==np.min(sensor_sq))
     min_ind=min_ind[0][0]
@@ -71,8 +57,8 @@ while (time.time()-t)<60:
         steer=0
             
     
-    v=1	#forward velocity
-    kp=0.5	#steering gain
+    v=1	
+    kp=0.5	
     vl=v+kp*steer
     vr=v-kp*steer
     print ("V_l =",vl)
@@ -81,10 +67,8 @@ while (time.time()-t)<60:
     errorCode=vrep.simxSetJointTargetVelocity(clientID,left_motor_handle,vl, vrep.simx_opmode_streaming)
     errorCode=vrep.simxSetJointTargetVelocity(clientID,right_motor_handle,vr, vrep.simx_opmode_streaming)
 
+    time.sleep(0.2) 
 
-    time.sleep(0.2) #loop executes once every 0.2 seconds (= 5 Hz)
-
-#Post ALlocation
 errorCode=vrep.simxSetJointTargetVelocity(clientID,left_motor_handle,0, vrep.simx_opmode_streaming)
 errorCode=vrep.simxSetJointTargetVelocity(clientID,right_motor_handle,0, vrep.simx_opmode_streaming)
     
